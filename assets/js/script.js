692e4a7bc7de23d51821c3b5c5dc877fb363eacc
Palloma Tiodosio <tiodosiop@gmail.com>
2025-09-04 19:33:09 -0300

    Atualização do Projeto

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,9 @@
   const botaoFecharMenu = document.querySelector('.fechar-menu');
   const formularioPesquisa = document.querySelector('.formulario-pesquisa');
   const textoPesquisar = document.querySelector('.texto-pesquisar');
-
+//   const textoPesquisar = document.querySelector('#textoPesquisar'); // sua palavra "Pesquisar"
+// const formularioPesquisa = document.querySelector('#formularioPesquisa');
+const inputPesquisa = formularioPesquisa.querySelector('input');
   // Abre o menu lateral
   botaoMenu.addEventListener('click', () => {
     menuLateral.classList.add('ativo');
@@ -16,9 +18,19 @@
   });
 
   // Alterna a barra de pesquisa
-  textoPesquisar.addEventListener('click', () => {
+ 
+textoPesquisar.addEventListener('click', () => {
     formularioPesquisa.classList.toggle('ativo');
+    
     if (formularioPesquisa.classList.contains('ativo')) {
-      formularioPesquisa.querySelector('input').focus();
+        // sumir com a palavra "Pesquisar"
+        textoPesquisar.style.display = 'none';
+        formularioPesquisa.querySelector('input').focus();
+    } else {
+        // mostrar de novo quando fechar
+        textoPesquisar.style.display = 'inline'; // ou 'block', dependendo do seu HTML
     }
-  });
+});
+
+
+
